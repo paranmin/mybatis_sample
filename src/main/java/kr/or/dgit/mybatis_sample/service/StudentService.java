c7ef22c01d312919740ea136e59af4b5927e813e
GangDae Lee <enhoomin@naver.com>
2018-02-09 11:11:10 +0900

    StudentDaoImpl 삭제

--- src/main/java/kr/or/dgit/mybatis_sample/service/StudentService.java
+++ src/main/java/kr/or/dgit/mybatis_sample/service/StudentService.java
@@ -2,25 +2,65 @@ package kr.or.dgit.mybatis_sample.service;
 
 import java.util.List;
 
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
 import org.apache.ibatis.session.SqlSession;
 
 import kr.or.dgit.mybatis_sample.dao.StudentDao;
-import kr.or.dgit.mybatis_sample.dao.StudentDaoImpl;
 import kr.or.dgit.mybatis_sample.dto.Student;
 import kr.or.dgit.mybatis_sample.util.MyBatisSqlSessionFactory;
 
 public class StudentService {
+	private static final Log log = LogFactory.getLog(StudentService.class);
+	private String namespace = "kr.or.dgit.mybatis_sample.dao.StudentDao.";
+	
 	public Student findStudent(Student student) {
+		log.debug("selectStudentByNo()");
 		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
-			StudentDao studentDao = new StudentDaoImpl(sqlSession);
+			StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
 			return studentDao.selectStudentByNo(student);
 		}
 	}
 	
 	public List<Student> findStudentByAll() {
+		log.debug("selectStudentByAll()");
 		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
-			StudentDao studentDao = new StudentDaoImpl(sqlSession);
+			StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
 			return studentDao.selectStudentByAll();
 		}
+	}
+	
+	public Student findStudentWithAPI(Student student) {
+		log.debug("selectStudentByNoWithAPI()");
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			return sqlSession.selectOne(namespace + "selectStudentByNoWithAPI", student);
+		}
+	}
+	
+	public List<Student> findStudentByAllWithAPI() {
+		log.debug("selectStudentByAllWithAPI()");
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			return sqlSession.selectList(namespace + "selectStudentByAllWithAPI");
+		}
+	}
+	
+	public int insertStudent(Student student) {
+		log.debug("insertStudent()");
+		int res = -1;
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+			res = studentDao.insertStudent(student);
+			sqlSession.commit();
+		}
+		return res;
+	}
+	public int insertStudentWithAPI(Student student) {
+		log.debug("insertStudentWithAPI()");
+		int res = -1;
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			res = sqlSession.insert(namespace + "insertStudentWithAPI", student);
+			sqlSession.commit();
+		}
+		return res;
 	}
 }
--- src/test/java/kr/or/dgit/mybatis_sample/StudentServiceTest.java
+++ src/test/java/kr/or/dgit/mybatis_sample/StudentServiceTest.java
@@ -2,18 +2,25 @@ package kr.or.dgit.mybatis_sample;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+import kr.or.dgit.mybatis_sample.dto.PhoneNumber;
 import kr.or.dgit.mybatis_sample.dto.Student;
 import kr.or.dgit.mybatis_sample.service.StudentService;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StudentServiceTest {
 	private static StudentService service;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		service = new StudentService();
@@ -40,5 +47,54 @@ public class StudentServiceTest {
 		for (Student std : listStd) {
 			System.out.println(std);
 		}
+	}
+
+	@Test
+	public void test3FindStudentWithAPI() {
+		Student std = new Student(1);
+		Student student = service.findStudentWithAPI(std);
+		assertNotNull(student);
+		assertEquals(std.getStudId(), student.getStudId());
+		System.out.println(student);
+	}
+
+	@Test
+	public void test4FindStudentAllWithAPI() {
+		List<Student> listStd = service.findStudentByAllWithAPI();
+		assertNotNull(listStd);
+		for (Student std : listStd) {
+			System.out.println(std);
+		}
+	}
+	
+	@Test
+	public void test5InsertStudent() {
+		Student std = new Student();
+		std.setStudId(3);
+		std.setName("홍길동1");
+		std.setEmail("hongidong1@test.com");
+		std.setPhone(new PhoneNumber("010-1111-2222"));
+		
+		Calendar newDate = GregorianCalendar.getInstance();
+		newDate.set(2018, 5, 10);
+		std.setDob(newDate.getTime());
+		
+		int res = service.insertStudent(std);
+		assertEquals(1, res);
+	}
+	
+	@Test
+	public void test6InsertStudentWithAPI() {
+		Student std = new Student();
+		std.setStudId(4);
+		std.setName("홍길동2");
+		std.setEmail("hongidong2@test.com");
+		std.setPhone(new PhoneNumber("010-3333-4444"));
+		
+		Calendar newDate = GregorianCalendar.getInstance();
+		newDate.set(2020, 5, 10);
+		std.setDob(newDate.getTime());
+		int res = service.insertStudentWithAPI(std);
+		assertEquals(1, res);
 	}
 }
