c17df7f3be020f237012e9eba58f9b63482db8af
GangDae Lee <enhoomin@naver.com>
2018-02-14 10:51:27 +0900

    Row 바운스 매핑
    AddressService
    AddressDao
    AddressMapper
    AddressServiceTest 추가

---
+++ src/main/java/kr/or/dgit/mybatis_sample/service/AddressService.java
@@ -0,0 +1,32 @@
+package kr.or.dgit.mybatis_sample.service;
+
+import java.util.List;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+
+import kr.or.dgit.mybatis_sample.dao.AddressDao;
+import kr.or.dgit.mybatis_sample.dto.Address;
+import kr.or.dgit.mybatis_sample.util.MyBatisSqlSessionFactory;
+
+public class AddressService {
+	private static AddressService instance = new AddressService();
+	private static final Log log = LogFactory.getLog(AddressService.class);
+	
+	public static AddressService getInstance() {
+		return instance;
+	}
+
+	private AddressService() {
+	}
+
+	public List<Address> selectAddressByAll(RowBounds rowBounds) {
+		log.debug("selectAddressByAll()");
+		try (SqlSession sqlSession = MyBatisSqlSessionFactory.openSession();) {
+			AddressDao addressDao = sqlSession.getMapper(AddressDao.class);
+			return addressDao.selectAddressByAll(rowBounds);
+		}
+	}
+}
